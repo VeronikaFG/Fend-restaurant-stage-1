4cef9ce398e4ed045cf30772cad37f4ee9ba9083
Veronica Giannotti <verogiann@libero.it>
2018-06-10 19:30:26 +0200

    Improve semantics

--- service.worker.js
+++ service.worker.js
@@ -35,7 +35,7 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request).then(function(response) {
-        return caches.open('rest-cache').then(function(cache) {
+        return caches.open('restcache').then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
         });
